b5f6fce4ef6ab7cb08d30bd690129439c21d6eb2
Sushruto Majumdar <sushruto26@gmail.com>
2026-04-19 12:36:30 +0530

    Remove hardcoded DB credentials

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
-import pg from "pg";
-
-export const pool = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "movie_db",
-  password: "Postgres@1234",
-  port: 5432,
+const pool = new pg.Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
